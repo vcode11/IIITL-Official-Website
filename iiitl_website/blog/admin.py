3f778d93db6dd684fa0e9b33031ceda8cea5297a
dojutsu-user <vaibhgupt199@gmail.com>
2019-02-05 12:44:12 +0530

    Edit admin options

--- iiitl_website/blog/admin.py
+++ iiitl_website/blog/admin.py
@@ -4,6 +4,10 @@ from blog.models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title', 'date_added', 'is_draft', 'is_published']
+    readonly_fields = ['date_added']
+    list_display_links = ['pk', 'title']
+    search_fields = ['title']
+    list_filter = ['is_draft', 'is_published', 'date_added', 'data_published']
 
 
 admin.site.register(Post, PostAdmin)
