5f81c4e8dabbd8e050a190ded51f016ea27a2459
vcode11 <40891269+vcode11@users.noreply.github.com>
2019-02-06 11:45:09 +0530

    Fix linting error

--- iiitl_website/blog/views.py
+++ iiitl_website/blog/views.py
@@ -5,9 +5,9 @@ from .models import Post
 
 def homepage(request):
     latest_posts = Post.objects.get_latest_posts()[:6]
-    return render(request, 'blog/index.html', {'latest_posts':latest_posts})
+    return render(request, 'blog/index.html', {'latest_posts': latest_posts})
 
 
 def post_lists(request):
     posts = Post.objects.get_latest_posts()
-    return render(request, 'blog/posts_list.html', {'posts':posts})
+    return render(request, 'blog/posts_list.html', {'posts': posts})
